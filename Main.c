320bb4f1773e946ddb5b8a14daebbc8c4e840af7
Jianing Tang <jt766@cornell.edu>
2020-04-27 14:51:03 -0500

    redirect, seq, pal

--- Main.c
+++ Main.c
@@ -1,11 +1,12 @@
-#include <limits.h>
-#include <stdio.h>
-#include <stdio.h>
-# include<string.h> 
-# include<stdlib.h> 
-# include <stdbool.h>
-# include<unistd.h> 
-# include<readline/readline.h> 
+#include<errno.h>
+#include<fcntl.h>
+#include<limits.h>
+#include<stdio.h>
+#include<string.h> 
+#include<stdlib.h> 
+#include <stdbool.h>
+#include<unistd.h> 
+#include<readline/readline.h> 
 
 # define MAX_INPUT_LEN 64
 
@@ -14,7 +15,7 @@ int getUserInput(char* userInput){
   fgets(buf, MAX_INPUT_LEN, stdin);
   char *token = strtok(buf, "\n");
   if (strlen(token)>MAX_INPUT_LEN){
-    // error
+    // error function
     return 1;
   }
   strcpy(userInput, buf);
@@ -57,6 +58,46 @@ int checkMixingCommand(char* userInput){
   }
 
 }
+int execPipe(char* userInput){
+
+}
+
+int execRedirec(char* userInput){
+    char* cmd = strtok(userInput, ">");
+    char* output_fd = strtok(NULL, " ");
+    // check invalid input eg. ls > out1 out2
+    char* extra = strtok(NULL, " ");
+    if(extra == NULL){
+     //error
+     return 1;
+    }
+    int fd1 = open("output_fd", O_RDWR); 
+    if (fd1 < 0)  { 
+        perror("Fail to open/ create file"); 
+        exit(1); 
+    }
+
+    int p=0;
+    char* parsed[20];
+    char* token = strtok(cmd, " ");
+    while(token != NULL){
+      parsed[p] = token;
+      token = strtok(cmd, " ");
+      p++;
+    }
+
+    dup2(fd1, STDOUT_FILENO);
+    if (execvp(parsed[0], parsed) < 0) { 
+      printf("\nCould not execute command.."); 
+    }
+
+    // Using close system Call 
+    if (close(fd1) < 0)  
+    { 
+        perror("Fail to open/ create file"); 
+        exit(1); 
+    } 
+}
 
 int parseSingleCommand(char* userInput){
   // case 1: pipeline
@@ -69,12 +110,42 @@ int parseSingleCommand(char* userInput){
 }
 
 int parseSequentialCommand(char* userInput){
-  printf("Sequential\n");
+  printf("--- Sequential Command\n");
+  int i, status;
+  pid_t pid;
+  // sequential way
+  char* token = strtok(userInput, ";");
+  while(token != NULL){
+    if((pid = fork())== 0){
+      parseSingleCommand(token);
+      exit(0);
+    }
+    else{
+      int cpid = waitpid(pid, &status, 0);
+      printf("Return child %d\n", cpid);
+    }
+    token = strtok(NULL, ";");
+  }
   return 0;
 }
 
 int parseParallelCommand(char* userInput){
-  printf("Parallel\n");
+  printf("--- Parallel Command\n");
+  int i, status;
+  pid_t pid;
+  char* token = strtok(userInput, "&");
+  while(token != NULL){
+    if((pid = fork())== 0){
+      parseSingleCommand(token);
+      exit(0);
+    }
+    token = strtok(NULL, "&");
+  }
+
+  while (wait(NULL) != -1){
+    pid_t cpid = waitpid(pid, &status, 0);
+    printf("Return child %d\n", cpid);
+  }
   return 0;
 }
 
@@ -106,7 +177,5 @@ int main(){
       printf("mixed command error");
       break;
   }
-
-
   return 0;
 }
