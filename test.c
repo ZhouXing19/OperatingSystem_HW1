030636272d0eeb47d0c5e2c529d4d27097349dfd
Jianing Tang <jt766@cornell.edu>
2020-04-30 23:53:28 -0500

    fix pipe

--- test.c
+++ test.c
@@ -1,3 +1,4 @@
+#include<ctype.h>
 #include<errno.h>
 #include<fcntl.h>
 #include<limits.h>
@@ -7,57 +8,22 @@
 #include<stdbool.h>
 #include<unistd.h> 
 #include<readline/readline.h> 
-
-#define MAX_INPUT_LEN 64
-
-int getUserInput(char* userInput){
-  char buf[MAX_INPUT_LEN]; 
-  fgets(buf, MAX_INPUT_LEN, stdin);
-  char *token = strtok(buf, "\n");
-  strcpy(userInput, buf);
+int exec(){
+  printf("Where is my miu? \n");
+  int fd = open("x2.txt", O_RDWR | O_CREAT | O_TRUNC);
+  if(fd>0){
+    dup2(fd, STDOUT_FILENO);
+    printf("Here is my miu\n");
+  }
   return 0;
 }
 
-int main(){ 
-  int inputFd = open("myFile", O_RDWR);
-  char* parsed1[20];
-  parsed1[0] = "ls";
-  parsed1[1] = "-la";
-
-  char* parsed2[20];
-  parsed2[0] = "echo";
-  printf("1HERE!");
-  char inbuf[1024];
-  int p[2], status, nbytes;
-  pid_t pid;
-  printf("HERE!");
-  if(pipe(p)<0) exit(1);
-  dup2(p[0], STDIN_FILENO); // read end of the pipe
-  // dup2(p[1], STDOUT_FILENO); // write end of the pipe
-  
-  // test p[0] == null
-  nbytes = read(p[0], inbuf, 1024);
-  printf("nbytes is %s\n", inbuf);
-  return 0;
-
-  if((pid = fork())== 0){
-      dup2(p[1], STDOUT_FILENO);
-      int res = execvp(parsed1[0], parsed1);
-      exit(0);
-    }
-  else{
-    int cpid = waitpid(pid, &status, 0);
+int main(){
+  int fd = open("x1.txt", O_RDWR | O_CREAT | O_TRUNC);
+  printf("before dup2");
+  if(fd>0){
+    dup2(fd, STDOUT_FILENO);
+    exec();
   }
-
-  if((pid = fork())== 0){
-      nbytes = read(p[0], inbuf, 1024);
-      dup2(STDOUT_FILENO, STDOUT_FILENO);
-      printf("get the output %s\n", inbuf);
-      exit(0);
-    }
-  else{
-    int cpid = waitpid(pid, &status, 0);
-    
-  }
-    return 0; 
+  printf("Out of scope");
 }
